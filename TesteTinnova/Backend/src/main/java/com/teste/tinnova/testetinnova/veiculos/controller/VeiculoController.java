b4ac1dd4a769fa8d6498e3cac80099e38c9ba86f
Silveira <jonas.silveira@wj.com.br>
2021-08-16 11:45:46 -0300

    Atualizado Swagger3 e outras dependencias com erro

--- TesteTinnova/Backend/src/main/java/com/teste/tinnova/testetinnova/veiculos/controller/VeiculoController.java
+++ TesteTinnova/Backend/src/main/java/com/teste/tinnova/testetinnova/veiculos/controller/VeiculoController.java
@@ -3,8 +3,11 @@ package com.teste.tinnova.testetinnova.veiculos.controller;
 import com.teste.tinnova.testetinnova.veiculos.dao.VeiculoDAO;
 import com.teste.tinnova.testetinnova.veiculos.model.Veiculo;
 import com.teste.tinnova.testetinnova.veiculos.service.VeiculoService;
-import io.swagger.annotations.ApiResponse;
-import io.swagger.annotations.ApiResponses;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
@@ -25,10 +28,13 @@ public class VeiculoController implements VeiculoDAO {
     @Override
     @GetMapping
     @ResponseBody
+    @Operation(summary = "Retorna lista de veiculos cadastrados")
     @ApiResponses(value = {
-            @ApiResponse(code = 200, message = "Retorna lista de veiculos cadastrados"),
-            @ApiResponse(code = 500, message = "Ocorreu um erro interno"),
-    })
+            @ApiResponse(responseCode = "200", description = "Retornou lista de veiculos",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = Veiculo.class)) }),
+            @ApiResponse(responseCode = "500", description = "Ocorreu um erro interno",
+                    content = @Content) })
     @ResponseStatus(HttpStatus.OK)
     public List<Veiculo> getAll() {
         return veiculoService.getAll();
@@ -38,11 +44,16 @@ public class VeiculoController implements VeiculoDAO {
     @GetMapping("/{id}")
     @ResponseBody
     @ResponseStatus(HttpStatus.OK)
+    @Operation(summary = "Retorna um veiculo pelo ip")
     @ApiResponses(value = {
-            @ApiResponse(code = 200, message = "Retorna um veiculo pelo ip"),
-            @ApiResponse(code = 500, message = "Ocorreu um erro interno"),
-            @ApiResponse(code = 404, message = "Id não encontrado")
-    })
+            @ApiResponse(responseCode = "200", description = "Achou o veiculo e o retornou",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = Veiculo.class)) }),
+            @ApiResponse(responseCode = "404", description = "Id não encontrado",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = Veiculo.class)) }),
+            @ApiResponse(responseCode = "500", description = "Ocorreu um erro interno",
+                    content = @Content) })
     public Veiculo getById(@PathVariable("id") @Valid String id) {
         return veiculoService.getById(id);
     }
@@ -51,10 +62,13 @@ public class VeiculoController implements VeiculoDAO {
     @GetMapping("/find")
     @ResponseBody
     @ResponseStatus(HttpStatus.OK)
+    @Operation(summary = "Retorna um ou mais veiculos dado uma pesquisa")
     @ApiResponses(value = {
-            @ApiResponse(code = 200, message = "Retorna um ou mais veiculos dado a pesquisa, deve se usar : para igual"),
-            @ApiResponse(code = 500, message = "Ocorreu um erro interno"), 
-    })
+            @ApiResponse(responseCode = "200", description = "Achou o veiculo e o retornou",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = Veiculo.class)) }),
+            @ApiResponse(responseCode = "500", description = "Ocorreu um erro interno",
+                    content = @Content) })
     public List<Veiculo> getByQuery(@RequestParam("q") @Valid String q) {
         return veiculoService.getByQuery(q);
     }
@@ -63,10 +77,13 @@ public class VeiculoController implements VeiculoDAO {
     @PostMapping
     @ResponseBody
     @ResponseStatus(HttpStatus.CREATED)
+    @Operation(summary = "Cria um novo veiculo e retorna o veiculo criado")
     @ApiResponses(value = {
-            @ApiResponse(code = 200, message = "Cria um novo veiculo e retorna o veiculo criado"),
-            @ApiResponse(code = 500, message = "Ocorreu um erro interno"),
-    })
+            @ApiResponse(responseCode = "201", description = "Criado com sucesso",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = Veiculo.class)) }),
+            @ApiResponse(responseCode = "500", description = "Ocorreu um erro interno",
+                    content = @Content) })
     public Veiculo create(@RequestBody @Valid Veiculo defaultClass) {
         return veiculoService.create(defaultClass);
     }
@@ -75,10 +92,16 @@ public class VeiculoController implements VeiculoDAO {
     @PutMapping("/{id}")
     @ResponseBody
     @ResponseStatus(HttpStatus.OK)
+    @Operation(summary =  "Atualiza todo o objeto veiculo")
     @ApiResponses(value = {
-            @ApiResponse(code = 200, message = "Atualiza todo o objeto veiculo"),
-            @ApiResponse(code = 500, message = "Ocorreu um erro interno"),
-    })
+            @ApiResponse(responseCode = "200", description = "Atualizado com sucesso",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = Veiculo.class)) }),
+            @ApiResponse(responseCode = "404", description = "Id não encontrado",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = Veiculo.class)) }),
+            @ApiResponse(responseCode = "500", description = "Ocorreu um erro interno",
+                    content = @Content) })
     public Veiculo updateAllFields(@PathVariable("id") @Valid String id,@RequestBody @Valid Veiculo defaultClass) {
         return veiculoService.updateAllFields(id, defaultClass);
     }
@@ -87,10 +110,16 @@ public class VeiculoController implements VeiculoDAO {
     @PatchMapping("/{id}")
     @ResponseBody
     @ResponseStatus(HttpStatus.OK)
+    @Operation(summary =  "Atualiza apenas um campo do objeto veiculo")
     @ApiResponses(value = {
-            @ApiResponse(code = 200, message = "Atualiza apenas um campo do objeto veiculo"),
-            @ApiResponse(code = 500, message = "Ocorreu um erro interno"),
-    })
+            @ApiResponse(responseCode = "200", description = "Atualizado com sucesso",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = Veiculo.class)) }),
+            @ApiResponse(responseCode = "404", description = "Id não encontrado",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = Veiculo.class)) }),
+            @ApiResponse(responseCode = "500", description = "Ocorreu um erro interno",
+                    content = @Content) })
     public Veiculo updateSomeFields(@PathVariable("id") @Valid String id,@RequestBody @Valid  Map<String, Object> fields){
        return veiculoService.updateSomeFields(id,fields);
     }
@@ -99,11 +128,16 @@ public class VeiculoController implements VeiculoDAO {
     @DeleteMapping("/{id}")
     @ResponseBody
     @ResponseStatus(HttpStatus.OK)
+    @Operation(summary =  "Deleta um veiculoo dado um id")
     @ApiResponses(value = {
-            @ApiResponse(code = 200, message = "Deleta um veiculo"),
-            @ApiResponse(code = 500, message = "Ocorreu um erro interno"),
-            @ApiResponse(code = 404, message = "Id não encontrado")
-    })
+            @ApiResponse(responseCode = "200", description = "Deletado com sucesso",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = Veiculo.class)) }),
+            @ApiResponse(responseCode = "404", description = "Id não encontrado",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = Veiculo.class)) }),
+            @ApiResponse(responseCode = "500", description = "Ocorreu um erro interno",
+                    content = @Content) })
     public String delete(@PathVariable String id) {
         return veiculoService.delete(id);
     }
@@ -112,10 +146,13 @@ public class VeiculoController implements VeiculoDAO {
     @GetMapping("/listDecade")
     @ResponseBody
     @ResponseStatus(HttpStatus.OK)
+    @Operation(summary = "Retorna uma lista de quantidade de veiculos por decada")
     @ApiResponses(value = {
-            @ApiResponse(code = 200, message = "Retorna uma lista de quantidade de veiculos por decada"),
-            @ApiResponse(code = 500, message = "Ocorreu um erro interno"), 
-    })
+            @ApiResponse(responseCode = "200", description = "Retornou lista de veiculos",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = Veiculo.class)) }),
+            @ApiResponse(responseCode = "500", description = "Ocorreu um erro interno",
+                    content = @Content) })
     public List<Map<String, Integer>>listDecade()  {
         return veiculoService.listDecade();
     }
@@ -125,10 +162,13 @@ public class VeiculoController implements VeiculoDAO {
     @GetMapping("/listMarca")
     @ResponseBody
     @ResponseStatus(HttpStatus.OK)
+    @Operation(summary = "Retorna uma lista de quantidade de veiculos por marca")
     @ApiResponses(value = {
-            @ApiResponse(code = 200, message = "Retorna uma lista de quantidade de veiculos por marca"),
-            @ApiResponse(code = 500, message = "Ocorreu um erro interno"),
-    })
+            @ApiResponse(responseCode = "200", description = "Retornou lista de veiculos",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = Veiculo.class)) }),
+            @ApiResponse(responseCode = "500", description = "Ocorreu um erro interno",
+                    content = @Content) })
     public List<Map<String, String>> listMarca() {
         return veiculoService.listMarca();
     }
@@ -137,10 +177,13 @@ public class VeiculoController implements VeiculoDAO {
     @GetMapping("/lastWeek")
     @ResponseBody
     @ResponseStatus(HttpStatus.OK)
+    @Operation(summary = "Retorna uma lista veiculos cadastrados na ultima semana")
     @ApiResponses(value = {
-            @ApiResponse(code = 200, message = "Retorna uma lista de quantidade de veiculos cadastrados na ultima semana"),
-            @ApiResponse(code = 500, message = "Ocorreu um erro interno"),
-    })
+            @ApiResponse(responseCode = "200", description = "Retornou lista de veiculos",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = Veiculo.class)) }),
+            @ApiResponse(responseCode = "500", description = "Ocorreu um erro interno",
+                    content = @Content) })
     public List<Veiculo>listAllfromLastWeek() {
         return veiculoService.listAllfromLastWeek();
     }
